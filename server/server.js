e399ad5754440dadccec18e549c97a8b9726c2b4
Shubham-kumar799 <skumar799874@gmail.com>
2022-04-02 15:05:55 +0530

    graphql and express server configured

--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-const { ApolloServer } = require('apollo-server-express');
-const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
-const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -8,44 +5,27 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config();
-const typeDefs = require('./graphQL/schema');
 const { readdirSync } = require('fs');
 
 const PORT = process.env.PORT || 8000;
 
-async function startApolloServer(typeDefs) {
-  const app = express();
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
-    mocks: true,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+const app = express();
 
-  //db
-  mongoose
-    .connect(process.env.MONGO_CONNECTION_URI)
-    .then(() => console.log('db connected 😁'))
-    .catch(error => console.log('error connecting db 😐', error));
+//APPLYING MIDDLEWARES
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cors());
+app.use(helmet());
 
-  await server.start();
+// CONNECTING TO DATABASE
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URI)
+  .then(() => console.log('db connected 😁'))
+  .catch(error => console.log('error connecting db 😐', error));
 
-  //applying middlewares
-  app.use(morgan('dev'));
-  app.use(bodyParser.json({ limit: '2mb' }));
-  app.use(cors());
-  app.use(helmet());
+//ROUTES
+readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)));
 
-  //routes
-  readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)));
-
-  server.applyMiddleware({ app });
-
-  await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
+app.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
-  console.log(
-    `📭 GraphQL endpoint at  http://localhost:${PORT}${server.graphqlPath}`
-  );
-}
-
-startApolloServer(typeDefs);
+});
