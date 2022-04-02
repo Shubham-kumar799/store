f0983993ea38a526e358051594daa26f623daedb
Shubham-kumar799 <skumar799874@gmail.com>
2022-04-02 15:05:16 +0530

    graphql and express server configured

---
+++ GraphQL/src/schema.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    getUsers: [User!]
+    getUser(id: ID!): User
+  }
+
+  "A user is any entity that user the app"
+  type User {
+    id: ID!
+    "Name of the user"
+    name: String!
+    "Email of the user"
+    email: String!
+    "Cart of the User"
+    cart: [String]
+    "Address of the user"
+    address: String
+    "Role of the User"
+    role: String
+  }
+`;
+
+module.exports = typeDefs;
